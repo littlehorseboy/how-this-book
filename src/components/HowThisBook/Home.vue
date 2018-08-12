113689ec1342dd24df24fc8936f0eb513ffbd9da
littlehorseboy <littlehorseboy@gmail.com>
2018-08-12 21:36:43 +0800

    feat: 首頁切版大致完成
    vuex 已裝, 只有 books, comments 等書籍詳細頁面做好再規劃
    
    footer 應該可重複使用 但是目前放在 Home.vue 之後再改
    
    todo: 一本書籍的詳細頁切版, webpack 圖片載入機制需要再深入了解
    
    issue 首頁

--- src/components/HowThisBook/Home.vue
+++ src/components/HowThisBook/Home.vue
@@ -2,124 +2,88 @@
   <div class="home mdc-typography">
     <mdc-layout-grid class="home__search-container"
       :style="{backgroundImage: `url(${require('../../assets/HowThisBook/book-863418_1920.jpg')})`}">
-      <!--佔格子讓中間能置中用-->
-      <mdc-layout-cell tablet=1 desktop=2 class="home__search-container__phone-hidden"></mdc-layout-cell>
+      <mdc-layout-cell tablet=1 desktop=2 class="home__search-container__phone-hidden">
+        <!-- mdc-layout-cell 佔格子製造假 padding 讓中間能置中用 -->
+      </mdc-layout-cell>
 
       <mdc-layout-cell phone=4 tablet=6 desktop=8 class="home__search-container__search-component">
         <div class="home__search-container__search-component__container">
           <mdc-display>好書客</mdc-display>
           <mdc-headline>好書客，一個分享讀書心得的平台。</mdc-headline>
           <div>
-            <mdc-button raised>找好書</mdc-button>
-            <mdc-button raised>找好作者</mdc-button>
+            <mdc-button raised @click="currentInput = 'book'">找好書</mdc-button>
+            <mdc-button raised @click="currentInput = 'author'">找作者</mdc-button>
           </div>
           <div>
-            <mdc-textfield v-model="searchBookName" label="找一本好書" box leading-icon="search" />
+            <mdc-textfield v-if="currentInput === 'book'" v-model="searchBookName" label="找一本好書" box leading-icon="search" />
+            <mdc-textfield v-if="currentInput === 'author'" v-model="searchBookName" label="找一個作者" box leading-icon="search" />
           </div>
         </div>
       </mdc-layout-cell>
 
-      <!--佔格子讓中間能置中用-->
-      <mdc-layout-cell tablet=1 desktop=2 class="home__mdc-layout-grid__phone-hidden"></mdc-layout-cell>
+      <mdc-layout-cell tablet=1 desktop=2 class="home__mdc-layout-grid__phone-hidden">
+        <!-- mdc-layout-cell 佔格子製造假 padding 讓中間能置中用 -->
+      </mdc-layout-cell>
     </mdc-layout-grid>
 
     <mdc-layout-grid class="home__book-container">
-      <!--佔格子讓中間能置中用-->
-      <mdc-layout-cell tablet=1 desktop=2 class="home__book-container__phone-hidden"></mdc-layout-cell>
+      <mdc-layout-cell tablet=1 desktop=2 class="home__book-container__phone-hidden">
+        <!-- mdc-layout-cell 佔格子製造假 padding 讓中間能置中用 -->
+      </mdc-layout-cell>
 
       <mdc-layout-cell phone=4 tablet=6 desktop=8 class="home__book-container__messages-component">
         <mdc-headline>熱門書籍</mdc-headline>
         <div v-show="currentTab === 'experience'">
-          <mdc-card class="home__book-container__messages-component__card">
-            <mdc-card-primary-action>
-              <mdc-card-text>
-                <!-- 內層 -->
-                <mdc-layout-grid>
-                  <!-- tablet=8 desktop=12 -->
-                  <mdc-layout-cell phone=4 tablet=3 desktop=3>
-                    <img src="https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/94/0010789427.jpg&v=5b1522bd&w=150&h=150">
-                  </mdc-layout-cell>
-                  <mdc-layout-cell phone=4 tablet=5 desktop=6>
-                    <mdc-body>重構 JavaScript</mdc-body>
-                    <mdc-body>ISBN: 9789864766826</mdc-body>
-                  </mdc-layout-cell>
-                  <mdc-layout-cell phone=4 tablet=8 desktop=3>
-                    <mdc-icon icon="star"></mdc-icon>
-                    <mdc-icon icon="star"></mdc-icon>
-                    <mdc-icon icon="star"></mdc-icon>
-                    <mdc-icon icon="star"></mdc-icon>
-                    <mdc-icon icon="star_border"></mdc-icon>
-                  </mdc-layout-cell>
-                  <!-- end tablet=8 desktop=12 -->
-                  <mdc-layout-cell phone=4 tablet=8 desktop=12>
-                    <mdc-body>作者: Evan Burchard</mdc-body>
-                    <mdc-body>譯者: 蔡存哲</mdc-body>
-                    <mdc-body>出版社: 歐萊禮</mdc-body>
-                    <mdc-body>出版日期: 2018/06/14</mdc-body>
-                  </mdc-layout-cell>
-                </mdc-layout-grid>
-                <!-- end 內層 -->
-              </mdc-card-text>
-              <mdc-card-actions>
-                <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
-                  <mdc-card-action-icon icon="thumb_up" />
-                </mdc-card-action-icons>
-                <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
-                  <mdc-card-action-icon icon="thumb_down" />
-                </mdc-card-action-icons>
-                <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
-                  <mdc-card-action-icon icon="comment" />
-                </mdc-card-action-icons>
-              </mdc-card-actions>
-            </mdc-card-primary-action>
-          </mdc-card>
-
-          <mdc-card class="home__book-container__messages-component__card">
-            <mdc-card-primary-action>
-              <mdc-card-text>
-                <!-- 內層 -->
-                <mdc-layout-grid>
-                  <!-- tablet=8 desktop=12 -->
-                  <mdc-layout-cell phone=4 tablet=3 desktop=3>
-                    <img src="https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/94/0010789427.jpg&v=5b1522bd&w=150&h=150">
-                  </mdc-layout-cell>
-                  <mdc-layout-cell phone=4 tablet=5 desktop=6>
-                    <mdc-body>重構 JavaScript</mdc-body>
-                    <mdc-body>ISBN: 9789864766826</mdc-body>
-                  </mdc-layout-cell>
-                  <mdc-layout-cell phone=4 tablet=8 desktop=3>
-                    <mdc-icon icon="star"></mdc-icon>
-                    <mdc-icon icon="star"></mdc-icon>
-                    <mdc-icon icon="star"></mdc-icon>
-                    <mdc-icon icon="star"></mdc-icon>
-                    <mdc-icon icon="star_border"></mdc-icon>
-                  </mdc-layout-cell>
-                  <!-- end tablet=8 desktop=12 -->
-                  <mdc-layout-cell phone=4 tablet=8 desktop=12>
-                    <mdc-body>作者: Evan Burchard</mdc-body>
-                    <mdc-body>譯者: 蔡存哲</mdc-body>
-                    <mdc-body>出版社: 歐萊禮</mdc-body>
-                    <mdc-body>出版日期: 2018/06/14</mdc-body>
-                  </mdc-layout-cell>
-                </mdc-layout-grid>
-                <!-- end 內層 -->
-              </mdc-card-text>
-              <mdc-card-actions>
-                <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
-                  <mdc-card-action-icon icon="thumb_up" />
-                </mdc-card-action-icons>
-                <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
-                  <mdc-card-action-icon icon="thumb_down" />
-                </mdc-card-action-icons>
-                <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
-                  <mdc-card-action-icon icon="comment" />
-                </mdc-card-action-icons>
-              </mdc-card-actions>
-            </mdc-card-primary-action>
-          </mdc-card>
+          <!-- v-for books -->
+          <div v-for="book in books" :key="book.id">
+            <mdc-card class="home__book-container__messages-component__card">
+              <mdc-card-primary-action>
+                <mdc-card-text>
+                  <!-- 書籍內容 -->
+                  <mdc-layout-grid>
+                    <!-- tablet=8 desktop=12 -->
+                    <mdc-layout-cell phone=4 tablet=3 desktop=3>
+                      <img :src="book.imgSrc">
+                    </mdc-layout-cell>
+                    <mdc-layout-cell phone=4 tablet=5 desktop=6>
+                      <mdc-body>{{ book.bookName }}</mdc-body>
+                      <mdc-body>ISBN: {{ book.ISBN }}</mdc-body>
+                    </mdc-layout-cell>
+                    <mdc-layout-cell phone=4 tablet=8 desktop=3>
+                      <mdc-icon icon="star"></mdc-icon>
+                      <mdc-icon icon="star"></mdc-icon>
+                      <mdc-icon icon="star"></mdc-icon>
+                      <mdc-icon icon="star"></mdc-icon>
+                      <mdc-icon icon="star_border"></mdc-icon>
+                    </mdc-layout-cell>
+                    <!-- end tablet=8 desktop=12 -->
+                    <mdc-layout-cell phone=4 tablet=8 desktop=12>
+                      <mdc-body>作者: {{ book.author }}</mdc-body>
+                      <mdc-body>譯者: {{ book.translator }}</mdc-body>
+                      <mdc-body>出版社: {{ book.publishing_house }}</mdc-body>
+                      <mdc-body>出版日期: {{ book.publish_date }}</mdc-body>
+                    </mdc-layout-cell>
+                  </mdc-layout-grid>
+                  <!-- end 書籍內容 -->
+                </mdc-card-text>
+                <mdc-card-actions>
+                  <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
+                    <mdc-card-action-icon icon="thumb_up" />
+                  </mdc-card-action-icons>
+                  <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
+                    <mdc-card-action-icon icon="thumb_down" />
+                  </mdc-card-action-icons>
+                  <mdc-card-action-icons class="home__book-container__messages-component__card__action-icons">
+                    <mdc-card-action-icon icon="comment" />
+                  </mdc-card-action-icons>
+                </mdc-card-actions>
+              </mdc-card-primary-action>
+            </mdc-card>
+          </div>
+          <!-- end v-for books -->
         </div>
 
-        <!-- 註解區域為 tabs 功能 -->
+        <!-- 以下註解區域為 tabs 功能 -->
         <!-- <mdc-tab-bar @change="hotTabsOnChange" class="home__book-container__messages-component__tabs">
           <mdc-tab>心得</mdc-tab>
           <mdc-tab>問答</mdc-tab>
@@ -178,61 +142,56 @@
         </div> -->
       </mdc-layout-cell>
 
-      <!--佔格子讓中間能置中用-->
-      <mdc-layout-cell tablet=1 desktop=2 class="home__book-container__phone-hidden"></mdc-layout-cell>
+      <mdc-layout-cell tablet=1 desktop=2 class="home__book-container__phone-hidden">
+        <!-- mdc-layout-cell 佔格子製造假 padding 讓中間能置中用 -->
+      </mdc-layout-cell>
     </mdc-layout-grid>
 
     <!-- footer -->
     <mdc-layout-grid class="home__footer-container">
-      <!--佔格子讓中間能置中用-->
-      <mdc-layout-cell tablet=1 desktop=2 class="home__footer-container__phone-hidden"></mdc-layout-cell>
+      <mdc-layout-cell tablet=1 desktop=2 class="home__footer-container__phone-hidden">
+        <!-- mdc-layout-cell 佔格子製造假 padding 讓中間能置中用 -->
+      </mdc-layout-cell>
 
-      <mdc-layout-cell phone=4 tablet=6 desktop=8 class="home__footer-container__search-component">
-        <div class="home__footer-container__search-component__container">
+      <mdc-layout-cell phone=4 tablet=6 desktop=8 class="home__footer-container__footer-component">
+        <div class="home__footer-container__footer-component__container">
           <div>
-            <mdc-button raised>找好書</mdc-button>
-            <mdc-button raised>找好作者</mdc-button>
+            <mdc-button raised @click="currentInput = 'book'">找好書</mdc-button>
+            <mdc-button raised @click="currentInput = 'author'">找作者</mdc-button>
           </div>
           <div>
-            <mdc-textfield v-model="searchBookName" label="找一本好書" box leading-icon="search" />
+            <mdc-textfield v-if="currentInput === 'book'" v-model="searchBookName" label="找一本好書" box leading-icon="search" />
+            <mdc-textfield v-if="currentInput === 'author'" v-model="searchBookName" label="找一個作者" box leading-icon="search" />
           </div>
           <mdc-body>
-            <mdc-button>關於我們</mdc-button>
-            <mdc-button>聯絡我們</mdc-button>
-            <mdc-button>FAQ</mdc-button>
-            <mdc-button>服務條款&隱私權政策</mdc-button>
+            <mdc-button raised>關於我們</mdc-button>
+            <mdc-button raised>聯絡我們</mdc-button>
+            <mdc-button raised>FAQ</mdc-button>
+            <mdc-button raised>服務條款&隱私權政策</mdc-button>
           </mdc-body>
           <mdc-body>© 2018 好書客 All Rights Reserved.</mdc-body>
         </div>
       </mdc-layout-cell>
 
-      <!--佔格子讓中間能置中用-->
-      <mdc-layout-cell tablet=1 desktop=2 class="home__mdc-layout-grid__phone-hidden"></mdc-layout-cell>
+      <mdc-layout-cell tablet=1 desktop=2 class="home__mdc-layout-grid__phone-hidden">
+        <!-- mdc-layout-cell 佔格子製造假 padding 讓中間能置中用 -->
+      </mdc-layout-cell>
     </mdc-layout-grid>
 
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'Home',
   data() {
     return {
-      searchBookName: '',
-      currentTab: 'experience',
-      books: [
-        {
-          id: 1,
-          ISBN: '',
-          bookName: '書',
-          imgSrc: 'https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/078/94/0010789427.jpg&v=5b1522bd&w=348&h=348',
-          author: '作者',
-          desc: '描述, 簡介',
-          publish_date: '出版日期',
-          create_at: '建立時間',
-        },
-      ],
-      comments: [
+      searchBookName: '', // 搜尋欄位值
+      currentInput: 'book', // 目前搜尋的 Input
+      currentTab: 'experience', // 目前分頁 (尚無使用)
+      comments: [ // comment 之後會放 vuex 裡吧
         {
           id: 1,
           book_id: 1,
@@ -243,6 +202,11 @@ export default {
       ],
     };
   },
+  computed: {
+    ...mapGetters({
+      books: 'getBooks',
+    }),
+  },
   methods: {
     hotTabsOnChange(idx) {
       // tabs 切換用
@@ -251,7 +215,6 @@ export default {
         'Q&A',
         'gossip',
       ];
-
       this.currentTab = tabsIndexArray[idx];
     },
   },
@@ -289,7 +252,7 @@ export default {
       }
     }
   }
-  // hot
+  // book
   &__book-container {
     background-color: #1c1a0914;
     &__phone-hidden {
@@ -320,18 +283,8 @@ export default {
   }
   // footer-container
   &__footer-container {
-    height: 600px;
-    // 圖片 webpack build 載入機制不夠明白
-    // background-image: url('../../../static/HowThisBook/book-863418_1920.jpg');
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
-    @media (max-width: 839px) {
-      height: 100vh;
-    }
-    >.mdc-layout-grid__inner {
-      height: 100%;
-    }
+    color: white;
+    background-color: var(--mdc-theme-primary, #212121);
     &__phone-hidden {
       @media (max-width: 479px) {
         display: none;
@@ -344,6 +297,9 @@ export default {
       &__container {
         text-align: center;
         color: white;
+        button {
+          color: white !important;
+        }
       }
     }
   }
