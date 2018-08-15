93bed4930c8fe72a19200ca65e589c860ef90c3c
littlehorseboy <littlehorseboy@gmail.com>
2018-08-15 22:39:28 +0800

    test: Home.vue
    vuex 利用 @vue/test-utils 解決
    
    todo: Home.vue 測試還有待完善
    
    issue test

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,21 +1,17 @@
-import Vue from 'vue';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import VueMDCAdapter from 'vue-mdc-adapter';
 import Vuex from 'vuex';
+import store from '@/store';
 import Home from '@/components/HowThisBook/Home';
 
-Vue.use(VueMDCAdapter);
+const localVue = createLocalVue();
+localVue.use(VueMDCAdapter);
+localVue.use(Vuex);
+const wrapper = shallowMount(Home, { store, localVue });
 
 describe('Home.vue', () => {
-  const Constructor = Vue.extend(Home);
-  const vm = new Constructor().$mount();
-  vm.use(Vuex);
-
-  it('vm === undefined', () => {
-    expect(vm).to.equal(undefined);
-  });
-
-  it('首頁最上面 .home__search-container 的 backgroundImage 是 book-863418_1920.jpg', () => {
-    expect(vm.$el.querySelector('.home > .home__search-container').style.backgroundImage)
-      .to.include('book-863418_1920.jpg');
+  it('首頁最上面 .home__search-container 的 backgroundImage 是 book-863418_1920', () => {
+    expect(wrapper.find('.home__search-container').element.style.backgroundImage)
+      .to.include('book-863418_1920');
   });
 });
